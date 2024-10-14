0887dd4eedaf68124b4b605553cc89013ce6ac16
MOA <moyo.ajla@gmail.com>
2024-10-14 13:54:23 +0100

    new discovery

--- utils/seedAdmin.js
+++ utils/seedAdmin.js
@@ -4,16 +4,16 @@ const seedAdmin = async ()=>{
 
     const Company ={name:'MOA_Animation_Studios', email:'moa.animationstudios@gmail.com', address: 'Lagos, Nigeria', status:'Active'}
 
-    const Admin = {name:'MASadmin', email:'admin@MAS.com', role:'Admin', phone: '+234-0000000000', password:'admin@MAS00', company_name:'MAS',company_address:'Lagos, Nigeria'}
+    const Admin = {name:'MASadmin', email:'admin@MAS.com', role:'Admin', phone: '+234-0000000000', password:'admin@MAS00'}
 
     try{
         const checkAdmin = await db.User.findAll({where: {role:'Admin'}})
 
-        const checkCompany = await db.Company.findAll({where:{email: Company.email}})
+        const checkCompany = await db.Company.findAll({where:{email: Company.email}}) //returns an array
 
         if ((checkAdmin.length === 0) && (checkCompany.length === 0)){    
 
-            const AdminCompany = await db.Company.create(Company)
+            const AdminCompany = await db.Company.create(Company) //returns an object 
 
             Admin.Companyid = AdminCompany.id
 
@@ -21,13 +21,14 @@ const seedAdmin = async ()=>{
     
             console.log("Seeded Admin")
         }else if (checkAdmin.length === 0){
-            Admin.Companyid = checkCompany.id
+            Admin.Companyid = checkCompany[0].id
 
             await db.User.create(Admin)
     
             console.log("Seeded Admin")
         }
     }catch(e){
+        console.log(e)
         console.log("error seeding admin")
     }
 }
